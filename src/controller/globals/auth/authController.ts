d958b13a217d2653e2427c893578d46ce70b2a9a
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-06-06 19:33:52 +0545

    minor changes

--- src/controller/globals/auth/authController.ts
+++ src/controller/globals/auth/authController.ts
@@ -15,44 +15,8 @@ reset password / otp
 import { Request, Response } from "express";
 import User from "../../../database/models/use.model";
 import bcrypt from "bcrypt";
-// ==============================
-// Functional approach (COMMENTED OUT)
-// ==============================
 
-/*
 
-const registerUser = async (req: Request, res: Response) => {
-  // Incoming data accept garne
-
-  // Functional approach
-  /**
-      const username=req.body.username
-      const password=req.body.password
-      const email=req.body.email
-   */
-  /*
-  const { username, password, email } = req.body;
-
-  // Check
-  if (!username || !password || !email) {
-    res.status(400).json({
-      message: "Please provide username, password, email",
-    });
-    return;
-  }
-
-  // Insert into user table
-  await User.create({
-    username,
-    password,
-    email,
-  });
-  res.status(200).json({
-    message: "Successfully registered user",
-  });
-};
-
-*/
 
 // ==============================
 // Class based approach (ACTIVE)
