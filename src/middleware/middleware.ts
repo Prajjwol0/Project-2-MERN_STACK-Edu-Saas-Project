599c755de39752262ebeb837644e934047264fcd
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-06-13 20:40:03 +0545

    changes

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,13 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import User from "../database/models/user.model";
 
-// Interface for extended request (currently empty, can be extended later)
-interface IExtendedRequest extends Request {}
+// Extended request interface to hold extra user data
+interface IExtendedRequest extends Request {
+  user?: {
+    name?: string;
+    age?: number;
+  };
+}
+
+// Interface for decoded JWT result
+interface IResultAayo {
+  id: string;
+  iat: number;
+  exp: number;
+}
 
 class Middleware {
 
   // Middleware function to check if user is logged in
-  static isLoggedIn(req: Request, res: Response, next: NextFunction) {
+  static isLoggedIn(req: IExtendedRequest, res: Response, next: NextFunction) {
     
     // Get token from request header
     const token = req.headers.authorization;
@@ -21,7 +34,7 @@ class Middleware {
     }
 
     // Verify the provided token
-    jwt.verify(token, 'thisissecret', (erroraayo, resultaayo) => {
+    jwt.verify(token, 'thisissecret', async (erroraayo, resultaayo: any) => {
       if (erroraayo) {
         // If token is invalid
         res.status(403).json({
@@ -31,7 +44,26 @@ class Middleware {
       } else {
         // If token is valid
         console.log(resultaayo);
-        next();  // proceed to next middleware/controller
+
+        // Find user by ID from token
+        const UserData = await User.findByPk(resultaayo.id);
+
+        // If user not found
+        if (!UserData) {
+          res.status(403).json({
+            message: "No user with that id, invalid token"
+          });
+          return;
+        } else {
+          // If user found, add extra data to request object
+          req.user = {
+            name: "this.name", // you can replace this.name with actual name
+            age: 23
+          };
+        }
+
+        // proceed to next middleware/controller
+        next();
       }
     });
   }
