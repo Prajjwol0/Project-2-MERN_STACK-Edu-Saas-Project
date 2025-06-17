82e1e238d0b269637f6aaaeff6457e34a6944581
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-06-17 19:39:53 +0545

    A minor change

--- src/route/institute/instituteRoute.ts
+++ src/route/institute/instituteRoute.ts
@@ -1,6 +1,6 @@
 
 import express, { Router } from "express"
-import createInstitute from "../../Institute/instituteController"
+import {createInstitute} from "../../Institute/instituteController"
 import Middleware from "../../middleware/middleware"
 
 const router:Router = express.Router()
